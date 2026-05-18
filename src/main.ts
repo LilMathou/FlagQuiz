8f5ab732eca9f41ffa80da4aa62f0c55e5947764
Mathis St-Onge <mathisstonge05@gmail.com>
2026-05-18 16:57:37 -0400

    Ajoute la structure de base

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import './assets/main.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap';
+
+createApp(App).mount('#app');
